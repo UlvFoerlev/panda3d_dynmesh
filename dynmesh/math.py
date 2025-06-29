d4c47d944c1112bcc6eb19eb4e2fc1a308b394d0
UlvFoerlev <ulvfoerlev@gmail.com>
2025-06-29 11:33:00 +0200

    Simple Rotation Matrix Test

--- dynmesh/math.py
+++ dynmesh/math.py
@@ -1,4 +1,4 @@
-from numpy import cross, eye, dot, ndarray
+from numpy import cross, eye, dot, ndarray, identity
 from scipy.linalg import expm, norm
 from panda3d.core import Vec3, Point3
 
@@ -6,6 +6,7 @@ from panda3d.core import Vec3, Point3
 def rotation_matrix(axis: Vec3, theta: float) -> ndarray:
     """
     Create a rotation matrix from axis and rotation theta.
+    Returns identity matrix if axis is a zero vector or if rotation is zero.
 
     Keyword arguments:
     axis -- 3D axis (Vec3)
@@ -15,6 +16,9 @@ def rotation_matrix(axis: Vec3, theta: float) -> ndarray:
     rotation matrix (ndarray)
 
     """
+    if axis.length() == 0 or theta == 0:
+        return identity(3)
+
     return expm(cross(eye(3), axis / norm(axis) * theta))
 
 
@@ -26,13 +30,14 @@ def rotate_point(
 
     Keyword arguments:
     point -- 3D point in space (Point3)
-    rotation_matrix -- Numpy rotation (NDarray) (see 'rotation_matrix' above)
+    rotation_matrix -- Numpy rotation.
+        (NDarray) (see 'rotation_matrix' above)
     pivot -- pivot point to rotate around, defaults to Vec3(0, 0, 0)
 
     Returns:
     rotated point (Point3)
-
     """
+
     pivot = pivot or Point3(0, 0, 0)
 
     return dot(rotation_matrix, point - pivot) + pivot  # type: ignore
