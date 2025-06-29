aa29a18804704149df897ba8ee996f91cabb1421
UlvFoerlev <ulvfoerlev@gmail.com>
2025-06-29 13:28:21 +0200

    Rotation Matrix Test

---
+++ dynmesh/tests/test_dyn_mesh__math.py
@@ -0,0 +1,44 @@
+import pytest
+
+# from numpy import array_equal, identity, ndarray, zeros, array
+import numpy as np
+from panda3d.core import Point3, Vec3
+
+from dynmesh.math import rotate_point, rotation_matrix
+
+
+@pytest.mark.parametrize(
+    argnames=["axis", "theta", "result_matrix"],
+    argvalues=[
+        (Vec3(1, 0, 0), 0, np.identity(3)),
+        (Vec3(0, 0, 0), 1.2, np.identity(3)),
+        (
+            Vec3(1, 0, 0),
+            1.2,
+            np.array(
+                [
+                    [1.0000000, 0.0000000, 0.0000000],
+                    [0.0000000, 0.36235771, -0.9320391],
+                    [0.0000000, 0.9320391, 0.36235771],
+                ]
+            ),
+        ),
+        (
+            Vec3(1, 3, 2),
+            2.1,
+            np.array(
+                [
+                    [
+                        [-0.3973571, -0.1389378, 0.9070852],
+                        [0.7838718, 0.4625550, 0.4142316],
+                        [-0.4771292, 0.8756365, -0.0748901],
+                    ]
+                ]
+            ),
+        ),
+    ],
+)
+def test_rotate_matrix(axis: Vec3, theta: float, result_matrix: np.ndarray):
+    m = rotation_matrix(axis=axis, theta=theta)
+
+    assert np.all(np.isclose(m, result_matrix))
